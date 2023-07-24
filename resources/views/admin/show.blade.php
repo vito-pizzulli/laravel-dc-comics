9bc3c7db186a6f3b709bc4469e45322cddc42f7e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-24 16:41:34 +0200

    Milestone 2 completed - each single comic infos are printed in page with show() operation

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -3,5 +3,20 @@
 @section('title', 'Laravel DC Comics Single Comic')
 
 @section('main-section')
-    <h2>This is a single comic element.</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>Title: {{ $comic->title }}</h2>
+            <img src="{{ $comic->thumb }}" alt="Comic Cover" class="w-25">
+            <p>ID: {{ $comic->id }}</p>
+            <p>Description: {{ $comic->description }}</p>
+            <p>Price: {{ $comic->price }}</p>
+            <p>Series: {{ $comic->series }}</p>
+            <p>Sale Date: {{ $comic->sale_date }}</p>
+            <p>Type: {{ $comic->type }}</p>
+            <p>Artists: {{ implode(', ', $comic->artists) }}.</p>
+            <p>Writers: {{ implode(', ', $comic->writers) }}.</p>
+        </div>
+    </div>
+</div>
 @endsection
