03b0ad1eb7f3c03a5d075095fdfb1697486b08fc
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-24 16:27:43 +0200

    added bootstrap table that displays each comic from comicsList most vital infos into the admin index, added view button to each row, with href pointing to the route that displays admin.show (where all the info of the single comic will be shown) and passing the single comic ID

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,5 +3,37 @@
 @section('title', 'Laravel DC Comics Index Panel')
 
 @section('main-section')
-    <h2>Welcome to the back office!</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Series</th>
+                        <th scope="col">Sale Date</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comicsList as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic->id }}</th>
+                            <td>{{ $comic->title }}</td>
+                            <td>{{ $comic->price }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->sale_date }}</td>
+                            <td>{{ $comic->type }}</td>
+                            <td><a class="btn btn-sm btn-primary me-2" href="{{ route('admin.show', $comic->id) }}">View</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    {{ $comicsList->links() }}
 @endsection
