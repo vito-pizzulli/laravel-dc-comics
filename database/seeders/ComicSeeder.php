a8d858a81ce74df47a10e5f72d3a890f55379a97
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-24 17:40:23 +0200

    Bonus completed - ComicSeeder.php finished and runned, db table populated correctly and without errors

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comicsList = config('comics');
+
+        foreach ($comicsList as $comic) {
+            Comic::create($comic);
+        }
     }
 }
