b804590522234cbac4d49bd39dca80438cb25dc6
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-25 17:31:13 +0200

    added dynamic active based of route name, added some scss styling to the nav links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,20 @@
-<header class="bg-primary text-white p-1 text-center">
+<header class="bg-primary p-1 text-center">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="DC Logo" style="width: 80px">
-            <h1 class="d-inline-block ms-3 fs-3">Laravel DC Comics Index Panel</h1>
+            <a href="{{ route('admin.index') }}">
+                <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="DC Logo" style="width: 80px">
+                <h1 class="d-inline-block ms-3 fs-3 text-white">Laravel DC Comics Index Panel</h1>
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" class="txt-white">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav w-100 justify-content-end">
                     <li class="nav-item">
-                        <a class="nav-link text-white fs-6" href="{{ route('admin.index') }}">Comics List</a>
+                        <a class="my_nav-link fs-5 {{ (Route::currentRouteName() == 'admin.index') ? 'active': ''}}" href="{{ route('admin.index') }}">Comics List</a>
                     </li>
                     <li class="nav-item ms-3">
-                        <a class="nav-link text-white fs-6" href="{{ route('admin.create') }}">Add New Comic</a>
+                        <a class="my_nav-link fs-5 {{ (Route::currentRouteName() == 'admin.create') ? 'active': ''}}" href="{{ route('admin.create') }}">Add New Comic</a>
                     </li>
                 </ul>
             </div>
