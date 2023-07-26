7048cec75f3c9764af5f66c23e76b49b9dccaa2c
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-26 15:42:05 +0200

    added destroy route for delete method in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::post('/', [AdminPageController::class, 'store'])->name('admin.store');
 Route::get('/{id}', [AdminPageController::class, 'show'])->name('admin.show');
 Route::get('/edit/{id}', [AdminPageController::class, 'edit'])->name('admin.edit');
 Route::put('/{id}', [AdminPageController::class, 'update'])->name('admin.update');
+Route::delete('/{id}', [AdminPageController::class, 'destroy'])->name('admin.destroy');
