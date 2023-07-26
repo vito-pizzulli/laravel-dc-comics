b2b370bf09f5d04794631e4b49234e0536135b54
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-26 17:05:17 +0200

    moved the delete confirm script from index blade to app.js so it can be used by all blades

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,14 @@ import '~resources/scss/app.scss';
 import.meta.glob([
     '../img/**'
 ]);
+
+const deleteForms = document.querySelectorAll('form.form-delete');
+deleteForms.forEach(deleteForm => {
+    deleteForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const deleteConfirm = window.confirm('Are you sure you want to delete this comic? This action is irreversible.');
+        if (deleteConfirm) {
+            this.submit();
+        }
+    });
+});
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -46,18 +46,3 @@
 
     {{ $comicsList->links() }}
 @endsection
-
-@section('custom-scripts-tail')
-    <script>
-        const deleteForms = document.querySelectorAll('form.form-delete');
-        deleteForms.forEach(deleteForm => {
-            deleteForm.addEventListener('submit', function(event) {
-                event.preventDefault();
-                const deleteConfirm = window.confirm('Are you sure you want to delete this comic? This action is irreversible.');
-                if (deleteConfirm) {
-                    this.submit();
-                }
-            });
-        });
-    </script>
-@endsection
